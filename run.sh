3513eaf9f4950661433c98ca0dbf381f43636331
kkontras <konstantinoskontras@gmail.com>
2024-03-04 14:52:36 +0100

    initial_commit

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 accelerate launch main_m.py --config "./configs/Kinetics/vit/audio_VAVL_test.json" --default_config "./configs/Kinetics/default_config_kinetics.json"
+#or
+python main_m.py --config "./configs/Kinetics/vit/audio_VAVL_test.json" --default_config "./configs/Kinetics/default_config_kinetics.json"
